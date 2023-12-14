355cc8b7c8c044500fe6e4e545e08c574edfd7df
mahf708 <naser.mahfouz@pnnl.gov>
2023-12-14 08:47:17 -0600

    add new outputs to aerocom aie

--- components/eam/src/physics/cam/output_aerocom_aie.F90
+++ components/eam/src/physics/cam/output_aerocom_aie.F90
@@ -36,25 +36,36 @@ module output_aerocom_aie
 !  variable index for pbuf
    integer :: angstrm_idx   ! Angstrom coefficients
    integer :: cdr_idx      ! droplet effective radius at top of liquid water clouds (meter)
+   integer :: cdr2_idx      ! droplet effective radius at top of liquid water clouds (meter)
    integer :: cdnc_idx     ! droplet number concentration in top layer of liquid water clouds (#/m3)
+   integer :: cdnc2_idx     ! droplet number concentration in top layer of liquid water clouds (#/m3)
    integer :: cdnum_idx    ! column-integrated droplet number concentrations
    integer :: icnum_idx    ! column-integrated ice crystal number concentrations
    integer :: clt_idx      ! fractional cover by all clouds
+   integer :: clt2_idx      ! fractional cover by all clouds
    integer :: lcc_idx      ! fractional cover by liquid water clouds
+   integer :: lcc2_idx      ! fractional cover by liquid water clouds
    integer :: lwp_idx      ! in-cloud liquid water path for liquid clouds (kg/m^2)
    integer :: iwp_idx      !  in-cloud ice water path for ice clouds (kg/m^2)
    integer :: icr_idx      ! effective radius of crystals at top of ice clouds (meter)
+   integer :: icr2_idx      ! effective radius of crystals at top of ice clouds (meter)
    integer :: icc_idx      ! fractional cover by ice clouds
+   integer :: icc2_idx      ! fractional cover by ice clouds
    integer :: cod_idx      ! in-cloud optical depth 
    integer :: ccn_idx      ! cloud condensation nuclei number concentration for liquid water
+   integer :: ccn2_idx      ! cloud condensation nuclei number concentration for liquid water
                                        ! clouds where activation corresponding to CDR and CDN
    integer :: ttop_idx     ! Temperature at top of clouds (K)
+   integer :: ttop2_idx     ! Temperature at top of clouds (K)
    integer :: htop_idx     ! Height at top of clouds (m)
+   integer :: htop2_idx     ! Height at top of clouds (m)
    integer :: ptop_idx     ! Pressure at top fo clouds (Pa)
+   integer :: ptop2_idx     ! Pressure at top fo clouds (Pa)
    integer :: autoconv_idx      ! Column-integrated autoconversion rate
    integer :: accretn_idx      ! Column-integrated autoconversion rate
    integer :: rh700_idx      ! relative humidity at 700 hPa
    integer :: icnc_idx     ! ice crystal number concentration in top layer of ice clouds (#/m3)
+   integer :: icnc2_idx     ! ice crystal number concentration in top layer of ice clouds (#/m3)
 
    integer :: intccn_idx   
    integer :: colrv_idx
@@ -86,24 +97,35 @@ CONTAINS
 
    call pbuf_add_field('angstrm',  'physpkg', dtype_r8, (/pcols/),      angstrm_idx)
    call pbuf_add_field('cdr',  'physpkg', dtype_r8, (/pcols/),      cdr_idx)
+   call pbuf_add_field('cdr2',  'physpkg', dtype_r8, (/pcols/),      cdr2_idx)
    call pbuf_add_field('cdnc',  'physpkg', dtype_r8, (/pcols/),      cdnc_idx)
+   call pbuf_add_field('cdnc2',  'physpkg', dtype_r8, (/pcols/),      cdnc2_idx)
    call pbuf_add_field('cdnum',  'physpkg', dtype_r8, (/pcols/),      cdnum_idx)
    call pbuf_add_field('icnum',  'physpkg', dtype_r8, (/pcols/),      icnum_idx)
    call pbuf_add_field('clt',  'physpkg', dtype_r8, (/pcols/),      clt_idx)
+   call pbuf_add_field('clt2',  'physpkg', dtype_r8, (/pcols/),      clt2_idx)
    call pbuf_add_field('lcc',  'physpkg', dtype_r8, (/pcols/),      lcc_idx)
+   call pbuf_add_field('lcc2',  'physpkg', dtype_r8, (/pcols/),      lcc2_idx)
    call pbuf_add_field('lwp',  'physpkg', dtype_r8, (/pcols/),      lwp_idx)
    call pbuf_add_field('iwp',  'physpkg', dtype_r8, (/pcols/),      iwp_idx)
    call pbuf_add_field('icr',  'physpkg', dtype_r8, (/pcols/),      icr_idx)
+   call pbuf_add_field('icr2',  'physpkg', dtype_r8, (/pcols/),      icr2_idx)
    call pbuf_add_field('icc',  'physpkg', dtype_r8, (/pcols/),      icc_idx)
+   call pbuf_add_field('icc2',  'physpkg', dtype_r8, (/pcols/),      icc2_idx)
    call pbuf_add_field('cod',  'physpkg', dtype_r8, (/pcols/),      cod_idx)
    call pbuf_add_field('ccn',  'physpkg', dtype_r8, (/pcols/),      ccn_idx)
+   call pbuf_add_field('ccn2',  'physpkg', dtype_r8, (/pcols/),      ccn2_idx)
    call pbuf_add_field('ttop', 'physpkg', dtype_r8, (/pcols/),      ttop_idx)
+   call pbuf_add_field('ttop2', 'physpkg', dtype_r8, (/pcols/),      ttop2_idx)
    call pbuf_add_field('htop', 'physpkg', dtype_r8, (/pcols/),      htop_idx)
+   call pbuf_add_field('htop2', 'physpkg', dtype_r8, (/pcols/),      htop2_idx)
    call pbuf_add_field('ptop', 'physpkg', dtype_r8, (/pcols/),      ptop_idx)
+   call pbuf_add_field('ptop2', 'physpkg', dtype_r8, (/pcols/),      ptop2_idx)
 
    call pbuf_add_field('autoconv', 'physpkg', dtype_r8, (/pcols/),      autoconv_idx)
    call pbuf_add_field('accretn', 'physpkg', dtype_r8, (/pcols/),      accretn_idx)
    call pbuf_add_field('icnc', 'physpkg', dtype_r8, (/pcols/),      icnc_idx)
+   call pbuf_add_field('icnc2', 'physpkg', dtype_r8, (/pcols/),      icnc2_idx)
    call pbuf_add_field('rh700', 'physpkg', dtype_r8, (/pcols/),      rh700_idx)
 
    call pbuf_add_field('intccn', 'physpkg', dtype_r8, (/pcols/),      intccn_idx)
@@ -138,29 +160,45 @@ CONTAINS
    call addfld ('aerindex', horiz_only ,'A' ,  '#', 'Aerosol Index (Angstrom coefficient * AOD)', flag_xyfill=.true.)
    call addfld ('cdr', horiz_only  ,'A'   ,   'meter', &
                'Grid-cell mean droplet effective radius at top of liquid water clouds',  flag_xyfill=.true.)
+   call addfld ('cdr2', horiz_only  ,'A'   ,   'meter', &
+               'Grid-cell mean droplet effective radius at top of liquid water clouds',  flag_xyfill=.true.)
    call addfld ('cdnc', horiz_only ,'A'    ,  '#/m3', &
+               'Grid-cell mean droplet number concentration at top of liquid water clouds',  flag_xyfill=.true.)
+   call addfld ('cdnc2', horiz_only ,'A'    ,  '#/m3', &
                'Grid-cell mean droplet number concentration at top of liquid water clouds',  flag_xyfill=.true.)
    call addfld ('cdnum', horiz_only ,'A'    ,  '#/m2', &
                'Grid-cell mean column-integrated droplet number concentrations',  flag_xyfill=.true.)
    call addfld ('icnum', horiz_only ,'A'    ,  '#/m2',  &
                'Grid-cell mean column-integrated ice crystal number concentrations',  flag_xyfill=.true.)
    call addfld ('clt', horiz_only ,'A'    ,   'fraction','Fractional cover by all clouds',   flag_xyfill=.true.) 
+   call addfld ('clt2', horiz_only ,'A'    ,   'fraction','Fractional cover by all clouds',   flag_xyfill=.true.) 
    call addfld ('lcc', horiz_only ,'A'    ,   'fraction','Fractional cover by liquid water clouds',   flag_xyfill=.true.)
+   call addfld ('lcc2', horiz_only ,'A'    ,   'fraction','Fractional cover by liquid water clouds',   flag_xyfill=.true.)
    call addfld ('lwp', horiz_only ,'A'    ,   'kg/m2',  &
                'Grid-cell mean liquid water path for liquid water clouds',  flag_xyfill=.true.)
    call addfld ('iwp', horiz_only ,'A'    ,   'kg/m2',  'Grid-cell mean ice water path for ice clouds',  flag_xyfill=.true.)
    call addfld ('icr', horiz_only ,'A'    ,   'meter',  &
                'Grid-cell mean effective radius of crystals at top of ice clouds',  flag_xyfill=.true.)
+   call addfld ('icr2', horiz_only ,'A'    ,   'meter',  &
+               'Grid-cell mean effective radius of crystals at top of ice clouds',  flag_xyfill=.true.)
    call addfld ('icc', horiz_only ,'A'    ,   'fracton','Fractional cover by ice clouds',  flag_xyfill=.true.)
+   call addfld ('icc2', horiz_only ,'A'    ,   'fracton','Fractional cover by ice clouds',  flag_xyfill=.true.)
    call addfld ('cod', horiz_only ,'A'    ,   'amount',  'Grid-cell mean cloud optical depth',  flag_xyfill=.true.)
    call addfld ('ccn', horiz_only ,'A'    ,   '#/m3',    &
                'CCN number concentration at 0.3% at the top layer of liquid water clouds',  flag_xyfill=.true.)
+   call addfld ('ccn2', horiz_only ,'A'    ,   '#/m3',    &
+               'CCN number concentration at 0.3% at the top layer of liquid water clouds',  flag_xyfill=.true.)
    call addfld ('ttop', horiz_only ,'A'    ,  'K',      'Temperature at top of clouds',  flag_xyfill=.true.)
+   call addfld ('ttop2', horiz_only ,'A'    ,  'K',      'Temperature at top of clouds',  flag_xyfill=.true.)
    call addfld ('htop', horiz_only ,'A'    ,  'm',      'Height at top of clouds',  flag_xyfill=.true.)
+   call addfld ('htop2', horiz_only ,'A'    ,  'm',      'Height at top of clouds',  flag_xyfill=.true.)
    call addfld ('ptop', horiz_only ,'A'    ,  'Pa',      'Pressure at top of clouds',  flag_xyfill=.true.)
+   call addfld ('ptop2', horiz_only ,'A'    ,  'Pa',      'Pressure at top of clouds',  flag_xyfill=.true.)
    call addfld ('autoconv', horiz_only ,'A'    ,  'kg/m2/s',   'Grid-mean surface precipitation rate',  flag_xyfill=.true.)
    call addfld ('accretn', horiz_only ,'A'    ,  'kg/m2/s',    'Grid-mean surface precipitation rate',  flag_xyfill=.true.)
    call addfld ('icnc', horiz_only ,'A'    ,  '#/m3',   &
+               'Ice crystal number concentration at top of ice clouds',  flag_xyfill=.true.)
+   call addfld ('icnc2', horiz_only ,'A'    ,  '#/m3',   &
                'Ice crystal number concentration at top of ice clouds',  flag_xyfill=.true.)
    call addfld ('rh700', horiz_only ,'A'    , 'fraction', 'Relative humidity at 700 hPa',  flag_xyfill=.true.)
 
@@ -258,16 +296,27 @@ CONTAINS
    real(r8), pointer, dimension(:, :) :: qrain  ! grid-mean rain water mixing ratio
 
    real(r8), pointer, dimension(:) ::  clt 
+   real(r8), pointer, dimension(:) ::  clt2 
    real(r8), pointer, dimension(:) ::  icc
+   real(r8), pointer, dimension(:) ::  icc2
    real(r8), pointer, dimension(:) ::  lcc
+   real(r8), pointer, dimension(:) ::  lcc2
    real(r8), pointer, dimension(:) ::  ttop 
+   real(r8), pointer, dimension(:) ::  ttop2
    real(r8), pointer, dimension(:) ::  htop
+   real(r8), pointer, dimension(:) ::  htop2
    real(r8), pointer, dimension(:) ::  ptop
+   real(r8), pointer, dimension(:) ::  ptop2
    real(r8), pointer, dimension(:) ::  cdr 
+   real(r8), pointer, dimension(:) ::  cdr2
    real(r8), pointer, dimension(:) ::  icr
+   real(r8), pointer, dimension(:) ::  icr2
    real(r8), pointer, dimension(:) ::  cdnc 
+   real(r8), pointer, dimension(:) ::  cdnc2
    real(r8), pointer, dimension(:) ::  icnc 
+   real(r8), pointer, dimension(:) ::  icnc2
    real(r8), pointer, dimension(:) ::  ccn
+   real(r8), pointer, dimension(:) ::  ccn2
 
    real(r8), pointer, dimension(:) :: cod
    real(r8), pointer, dimension(:) :: lwp
@@ -291,6 +340,7 @@ CONTAINS
 
 
    real(r8) :: ftmp(pcols)
+   real(r8) :: ftmp2(pcols)
    real(r8) :: zi(pver+1) 
    real(r8) :: flag_max 
    real(r8) :: thres_cld, thres_cod, thres_cwp, max_cld, fr, cwp
@@ -331,16 +381,27 @@ CONTAINS
    call pbuf_get_field(pbuf, numicebf_idx, numicebf)
 
    call pbuf_get_field(pbuf, clt_idx, clt)
+   call pbuf_get_field(pbuf, clt2_idx, clt2)
    call pbuf_get_field(pbuf, icc_idx, icc)
+   call pbuf_get_field(pbuf, icc2_idx, icc2)
    call pbuf_get_field(pbuf, lcc_idx, lcc)
+   call pbuf_get_field(pbuf, lcc2_idx, lcc2)
    call pbuf_get_field(pbuf, ttop_idx, ttop)
+   call pbuf_get_field(pbuf, ttop2_idx, ttop2)
    call pbuf_get_field(pbuf, htop_idx, htop)
+   call pbuf_get_field(pbuf, htop2_idx, htop2)
    call pbuf_get_field(pbuf, ptop_idx, ptop)
+   call pbuf_get_field(pbuf, ptop2_idx, ptop2)
    call pbuf_get_field(pbuf, cdr_idx, cdr)
+   call pbuf_get_field(pbuf, cdr2_idx, cdr2)
    call pbuf_get_field(pbuf, icr_idx, icr)
+   call pbuf_get_field(pbuf, icr2_idx, icr2)
    call pbuf_get_field(pbuf, cdnc_idx, cdnc)
+   call pbuf_get_field(pbuf, cdnc2_idx, cdnc2)
    call pbuf_get_field(pbuf, icnc_idx, icnc)
+   call pbuf_get_field(pbuf, icnc2_idx, icnc2)
    call pbuf_get_field(pbuf, ccn_idx, ccn)
+   call pbuf_get_field(pbuf, ccn2_idx, ccn2)
    call pbuf_get_field(pbuf, cod_idx, cod)
    call pbuf_get_field(pbuf, lwp_idx, lwp)
    call pbuf_get_field(pbuf, iwp_idx, iwp)
@@ -371,21 +432,70 @@ CONTAINS
    cld(:, :) = cldo(:, :)
    if ( iovl.eq.2 .or.iovl.eq.3 ) then 
     clt(:) = 1._r8
+    clt2(:) = 1._r8
    else 
     clt(:) = 0.0_r8
+    clt2(:) = 0.0_r8
    end if 
    icc(:) = 0.0_r8
+   icc2(:) = 0.0_r8
    lcc(:) = 0.0_r8
+   lcc2(:) = 0.0_r8
    ttop(:) = 0.0_r8
+   ttop2(:) = 0.0_r8
    htop(:) = 0.0_r8
+   htop2(:) = 0.0_r8
    ptop(:) = 0.0_r8
+   ptop2(:) = 0.0_r8
    cdr(:) = 0.0_r8
+   cdr2(:) = 0.0_r8
    icr(:) = 0.0_r8
+   icr2(:) = 0.0_r8
    cdnc(:) = 0.0_r8
+   cdnc2(:) = 0.0_r8
    icnc(:) = 0.0_r8
+   icnc2(:) = 0.0_r8
    ccn(:) = 0.0_r8
+   ccn2(:) = 0.0_r8
    lwp2(:) = 0.0_r8
    iwp2(:) = 0.0_r8
+
+   if ( iovl == 3 ) then
+      do i=1, ncol
+         do k=2, pver
+            ! cloud water content (liq and ice)
+            cwp =  ( cldliq(i,k) + cldice(i,k) ) * state%pdel(i,k)/gravit
+            if ( cwp > 0.0 .and. cld(i,k) > thres_cld ) then
+
+               fr =  ( 1._r8 - max( cld(i,k-1), cld(i,k) ) ) / &
+                     ( 1._r8 - min( cld(i,k-1), 1._r8 - thres_cld ) )
+
+               ftmp2(i) = clt2(i) * fr
+
+               ttop2(i) = ttop2(i) + state%t(i,k) * ( clt2(i) - ftmp2(i) ) 
+               htop2(i) = htop2(i) + state%t(i,k) * ( clt2(i) - ftmp2(i) ) 
+               ptop2(i) = ptop2(i) + state%pmid(i,k) * ( clt2(i) - ftmp2(i) )
+
+               lphase = cldliq(i,k) / ( cldliq(i,k) + cldice(i,k) )
+               iphase = 1.0_r8 - lphase
+
+               ! ice clouds
+               icr2(i) = icr2(i) + rei(i,k) * iphase * ( clt2(i) - ftmp2(i) ) * 1.0e-6  ! um --> m
+               icc2(i) = icc2(i) + iphase * ( clt2(i) - ftmp2(i) )
+               icnc2(i) = icnc2(i) + ind(i,k) * iphase * ( clt2(i) - ftmp2(i) )
+      
+               ! liquid water clouds
+               cdr2(i) = cdr2(i) + rel(i,k) * lphase * ( clt2(i) - ftmp2(i) )  * 1.0e-6  ! um -> m
+               cdnc2(i) = cdnc2(i) + lnd(i,k) * lphase * ( clt2(i) - ftmp2(i) ) 
+               ccn2(i)  = ccn2(i) + ccn3d(i,k) * lphase * ( clt2(i) - ftmp2(i)) * 1.0e6  ! #/cm3 --> #/m3
+               lcc2(i) = lcc2(i) + lphase * ( clt2(i) - ftmp2(i) )
+
+               clt2(i) = ftmp2(i)
+            end if ! cwp > 0 and cld(i,k) > thres_cld
+         end do ! k=2, pver
+         clt2(i) = 1._r8 - clt2(i)
+      end do ! i=1, ncol
+   end if ! iovl == 3
 
    do i=1, ncol
     do k=2, pver ! assumption: uppermost layer is cloud-free (k=1)
@@ -557,21 +667,32 @@ CONTAINS
    end do
 
    call outfld('cdr', cdr, pcols, lchnk) 
+   call outfld('cdr2', cdr2, pcols, lchnk) 
    call outfld('cdnc', cdnc, pcols, lchnk)
+   call outfld('cdnc2', cdnc2, pcols, lchnk)
    call outfld('cdnum', cdnum, pcols, lchnk)
    call outfld('icnum', icnum, pcols, lchnk)
    call outfld('clt', clt, pcols, lchnk)
+   call outfld('clt2', clt2, pcols, lchnk)
    call outfld('lcc', lcc, pcols, lchnk)
+   call outfld('lcc2', lcc2, pcols, lchnk)
    call outfld('lwp', lwp, pcols, lchnk)
    call outfld('iwp', iwp, pcols, lchnk)
    call outfld('icc', icc, pcols, lchnk)
+   call outfld('icc2', icc2, pcols, lchnk)
    call outfld('icnc', icnc, pcols, lchnk)
+   call outfld('icnc2', icnc2, pcols, lchnk)
    call outfld('icr', icr, pcols, lchnk)
+   call outfld('icr2', icr2, pcols, lchnk)
    call outfld('cod', cod, pcols, lchnk)
    call outfld('ccn', ccn, pcols, lchnk)
+   call outfld('ccn2', ccn2, pcols, lchnk)
    call outfld('ptop', ptop, pcols, lchnk)
+   call outfld('ptop2', ptop2, pcols, lchnk)
    call outfld('ttop', ttop, pcols, lchnk)
+   call outfld('ttop2', ttop2, pcols, lchnk)
    call outfld('htop', htop, pcols, lchnk)
+   call outfld('htop2', htop2, pcols, lchnk)
    call outfld('intccn', intccn, pcols, lchnk)
    call outfld('colrv', colrv, pcols, lchnk)
    call outfld('rwp', rwp, pcols, lchnk)
