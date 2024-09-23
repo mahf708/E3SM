6eca7d1d4cdafd8ef9f4977a45fd6b42d91dc216
Naser Mahfouz <naser.mahfouz@pnnl.gov>
2024-09-23 02:31:45 -0400

    make it work

--- components/eamxx/cmake/machine-files/ghci-oci.cmake
+++ components/eamxx/cmake/machine-files/ghci-oci.cmake
@@ -1,2 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 common_setup()
+
+set(CMAKE_Fortran_FLAGS "-Wno-maybe-uninitialized -Wno-unused-dummy-argument -fallow-argument-mismatch"  CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-fvisibility-inlines-hidden -fmessage-length=0 -Wno-use-after-free -Wno-unused-variable -Wno-maybe-uninitialized" CACHE STRING "" FORCE)
+
+set(EKAT_MPI_NP_FLAG "-np" CACHE STRING "-np")
+
+set(ENV{CCSM_CPRNC} "/usr/local/packages/bin/cprnc")
